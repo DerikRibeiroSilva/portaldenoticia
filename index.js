ed5d1bccd93ade0f5e5e887195677db6982e3fe2
DerikRibeiroSilva <derik.ribeiro10@gmail.com>
2021-03-30 12:44:37 -0300

    Organizando o Front

--- index.js
+++ index.js
@@ -5,25 +5,27 @@ const path = require('path');
 
 const app = express();
 
-app.use( bodyParser.json());
-app.use( bodyParser.urlencoded({
-    extended: true
-}));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use('/public', express.static(path.join(__dirname,'public')));
-app.set('view', path.join(__dirname, '/views'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/pages'));
 
 
 app.get('/',(req,res)=>{
     
     if(req.query.busca == null){
-        res.send('home');
+        res.render('home',{});
     }else{
         res.send('Você buscou: '+req.query.busca);
     }
-})
+
+  
+});
 
 
 app.get('/:slug',(req,res)=>{
@@ -32,7 +34,6 @@ app.get('/:slug',(req,res)=>{
 
 
 
-
-server.listen(port,hostname,() =>{
-    console.log("Servidor está rodando");
+app.listen(5000,()=>{
+    console.log('server rodando!');
 })
